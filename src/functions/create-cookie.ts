6543fef5c308f3fb6fca96e9f44e7fe7187d7f69
Venel <erazzorlinx@gmail.com>
2026-01-31 15:29:53 +0100

    refactor: Simplify create-cookie function

--- src/functions/create-cookie.ts
+++ src/functions/create-cookie.ts
@@ -1,8 +1,6 @@
 import fs from "fs/promises"
-import { createErrorEmbed } from "../embeds/common"
-import { ChatInputCommandInteraction } from "discord.js"
 
-export default async function createCookie(interaction: ChatInputCommandInteraction) {
+export default async function createCookie() {
     try {
         const user = await fs.readFile("./src/user/user.json", 'utf-8')
         const userb64 = Buffer.from(user, "utf-8").toString('base64url')
@@ -10,10 +8,7 @@ export default async function createCookie(interaction: ChatInputCommandInteract
         const cookie = `sb-${process.env.SUPABASE_PROJECT_ID}-auth-token=base64-${userb64}`
         return cookie
     } catch(err) {
-        const errorEmbed = createErrorEmbed(
-            "Erreur de génération",
-            "Le cookie n'a pas pu être généré. Veuillez réessayer plus tard.\n Si le problème persiste, contactez @<580447236702470176>"
-        )
-        return await interaction.reply({embeds: [errorEmbed]})
+        console.error("Erreur de génération, Le cookie n'a pas pu être généré", err)
+        return 
     }
 }
